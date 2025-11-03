55eb40c26b0ee295fc2c8ceee81592c663d0a6bf
AwesomeSauce-67 <750433@student.dusd.net>
2025-11-03 15:28:29 -0800

    Movement and added tiles
    Added WASD movement and added tiles.

--- P7GarciaCristian2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7GarciaCristian2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,8 +14,30 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f;
+        if (Keyboard.current.aKey.isPressed)
+        {
+            horizontal = -0.1f;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            horizontal = 0.1f;
+        }
+        float vertical = 0.0f;
+        if (Keyboard.current.wKey.isPressed)
+        {
+            vertical = 0.1f;
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            vertical = -0.1f;
+        }
+
+        Debug.Log(horizontal);
+
         Vector2 position=transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f*horizontal;
+        position.y = position.y +0.1f*vertical;
         transform.position = position;
     }
 }
