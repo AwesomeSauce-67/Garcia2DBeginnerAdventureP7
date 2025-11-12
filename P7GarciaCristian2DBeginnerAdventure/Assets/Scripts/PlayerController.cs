b568241492e3f938155ab43307e6314ad87e2ef6
AwesomeSauce-67 <750433@student.dusd.net>
2025-11-12 13:59:32 -0800

    a

--- P7GarciaCristian2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7GarciaCristian2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -5,39 +5,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        if (Keyboard.current.aKey.isPressed)
-        {
-            horizontal = -0.1f;
-        }
-        else if (Keyboard.current.dKey.isPressed)
-        {
-            horizontal = 0.1f;
-        }
-        float vertical = 0.0f;
-        if (Keyboard.current.wKey.isPressed)
-        {
-            vertical = 0.1f;
-        }
-        else if (Keyboard.current.sKey.isPressed)
-        {
-            vertical = -0.1f;
-        }
-
-        Debug.Log(horizontal);
-
-        Vector2 position=transform.position;
-        position.x = position.x + 0.1f*horizontal;
-        position.y = position.y +0.1f*vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.01f;
         transform.position = position;
     }
 }
