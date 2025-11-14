112a319c1423af15a1456b6dd03fda6daf9bbafe
AwesomeSauce-67 <750433@student.dusd.net>
2025-11-14 15:29:05 -0800

    Prefabs and rigidbody's
    Added/made sprites into prefabs and added physics to sprites.

--- P7GarciaCristian2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7GarciaCristian2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,28 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    Rigidbody rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
+        Vector2 move;
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
         Vector2 position = (Vector2)transform.position + move * 3f*Time.deltaTime;
         transform.position = position;
+    }
+    private void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+
     }
 }
